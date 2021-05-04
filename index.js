88c4c985afbd751992888bde9abf0e6f3e49b0fe
Laura Messa Travin <laura.travin@gmail.com>
2021-05-04 00:21:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Your code here
+// function mapToNegativize(sArray){
+//   return sArray.map( x => x * -1)
+// }
+
 function mapToNegativize(sArray){
-  return sArray.map( x => x * -1)
+   const newA=[]
+   for (const num of sArray){
+     newA.push(num *- 1)
+   }
+
+  return newA
 }
